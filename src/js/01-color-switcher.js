5af9965451287bf36c7d2eaf2c48e14e7287be96
Serhii Yevchishen <magodza06@gmail.com>
2023-03-07 16:44:16 +0200

    add-js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const bodyColor = document.querySelector('body');
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+const btnColor = document.querySelector('.back-btn');
+let intervalId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+buttonStop.setAttribute('disabled', '');
+
+buttonStart.addEventListener('click', element => {
+  element.target.setAttribute('disabled', true);
+  buttonStop.removeAttribute('disabled');
+
+  intervalId = setInterval(() => {
+      bodyColor.style.backgroundColor = getRandomHexColor();
+      btnColor.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+buttonStop.addEventListener('click', element => {
+  element.target.setAttribute('disabled', true);
+  buttonStart.removeAttribute('disabled');
+
+  clearInterval(intervalId);
+});
